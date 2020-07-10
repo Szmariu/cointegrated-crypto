e8b6ed3169b90dc9331e052c70282cae043cb5d8
Michał Szałański <michal.szalanski1@gmail.com>
2020-07-10 18:50:21 +0200

    Add basic EDA

--- code.R
+++ code.R
@@ -1,13 +1,22 @@
-# function call
+### Preparation
+# Libraries
 library(tidyverse)
+library(ggplot2) # nice plots
+library(ggthemes) 
+library(ggtech) # For the airbnb theme
 
+# For better plotting
+theme_set(theme_airbnb_fancy())
+orange = "#FFB400"
+purple = "#7B0051"
+
+# Fix for the month shorthands
 Sys.setlocale("LC_ALL","English")
 
-getCryptoHistoricalPrice <- function(x){
-	# this function scraps the OHLC historical crypto prices from www.coinmarketcap.com
-	library(tidyverse)
+# This function scraps the OHLC historical crypto prices from www.coinmarketcap.com
+getCryptoHistoricalPrice <- function(currency){
 	paste0("https://coinmarketcap.com/currencies/",
-		   x,
+		   currency,
 		   "/historical-data/?start=20170101&end=21000101") %>%
 		xml2::read_html() %>%
 		rvest::html_table(.) %>%
@@ -22,20 +31,63 @@ getCryptoHistoricalPrice <- function(x){
 		return()
 }
 
-
+### Data downloading
 btc <- getCryptoHistoricalPrice("bitcoin")
-summary(btc)
+eth <- getCryptoHistoricalPrice("ethereum")
+
+### Basic ETA
+# Shape
 btc %>% tail
+eth %>% tail
+
+# Summary
+summary(btc)
+summary(eth)
+
+# Basic plots
 btc %>%
 	ggplot(aes(Date, Close)) +
-	geom_line() +
-	labs(title = "Bitcoin historical prices")
+	geom_area(fill = orange, color = orange, alpha = 0.5) +
+	labs(title = "Bitcoin historical prices") +
+	theme(
+		text = element_text(family = "sans"), # Remove the font warnings
+		plot.title = element_text(family = "sans")
+	)
 
-
-eth <- getCryptoHistoricalPrice("ethereum")
-summary(eth)
-eth %>% tail
 eth %>%
 	ggplot(aes(Date, Close)) +
+	geom_area(fill = orange, color = orange, alpha = 0.5) +
+	labs(title = "Etherium historical prices") +
+	theme(
+		text = element_text(family = "sans"), # Remove the font warnings
+		plot.title = element_text(family = "sans")
+	)
+
+btc$source <- 'Bitcoin'
+eth$source <- 'Etherium'
+
+btc$ClosePercent <- btc$Close / max(btc$Close)
+eth$ClosePercent <- eth$Close / max(eth$Close)
+
+
+bind_rows(btc, eth) %>%
+	ggplot(aes(Date, 
+			   ClosePercent,
+			   color = source)) +
 	geom_line() +
-	labs(title = "Etherium historical prices")
+	scale_color_manual(values = c(orange, purple)) +
+	labs(title = "Close price as % of max price for each coin") +
+	theme(
+		text = element_text(family = "sans"), # Remove the font warnings
+		plot.title = element_text(family = "sans")
+	)
+
+bind_rows(btc, eth) %>%
+	ggplot(aes(Date, ClosePercent)) +
+	geom_line(color = orange) +
+	labs(title = "Close price as % of max price for each coin",
+		 subtitle = 'Colored only the difference') +
+	theme(
+		text = element_text(family = "sans"), # Remove the font warnings
+		plot.title = element_text(family = "sans")
+	)
